bc2ea654e1999aac3249455eb63a1c82695c5760
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2023-03-28 00:19:56 +0200

    Move header and paragraph inside new div

--- src/components/qrcode/QRCode.tsx
+++ src/components/qrcode/QRCode.tsx
@@ -6,11 +6,12 @@ export default function QRCode() {
     <div className='qr-code-project'>
       <div className="card">
         <img src={QRCodeImage} alt="QR Code" />
-        <h1>Improve your front-end skills by building projects</h1>
-        <p>
-          Scan the QR code to visit Frontend Mentor
-          and take your coding skills to the next level
-        </p>
+        <div>
+          <h1>Improve your front-end skills by building projects</h1>
+          <p>
+            Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+          </p>
+        </div>
       </div>
     </div>
   )

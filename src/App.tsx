ec00fbb43f7540f3faeb0c7b99ca84643cec92bf
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2023-03-31 00:14:31 +0200

    Add News Homepage project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { Routes, Route } from 'react-router-dom'
+import './main.sass'
 import QRCode from './frontend_mentor_projects/qrcode/QRCode'
 import ResultsSummary from './frontend_mentor_projects/results_summary/ResultsSummary'
 import Home from './components/Home'
-import './main.sass'
+import NewsHomepage from './frontend_mentor_projects/news_homepage/NewsHomepage'
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/qrcode" element={<QRCode />} />
         <Route path="/results-summary" element={<ResultsSummary />} />
+        <Route path="/news-homepage" element={<NewsHomepage />} />
       </Routes>
     </div>
   )
--- src/components/ProjectCards.tsx
+++ src/components/ProjectCards.tsx
@@ -2,6 +2,7 @@ import '../styling/ProjectCards.sass'
 import { Link } from 'react-router-dom'
 import QRCodePreviewImage from '../assets/project_previews/QR_code.png'
 import ResultsSummaryPewviewImage from '../assets/project_previews/ResultsSummary.png'
+import NewsHomepage from '../assets/project_previews/NewsHomepage.png'
 
 export default function ProjectCards() {
   
@@ -28,6 +29,12 @@ export default function ProjectCards() {
       description: 'Another simple Frontend Mentor challenge. This is a card showing a summary from the results of something such as an assessment.',
       url: 'results-summary',
       imageURL: ResultsSummaryPewviewImage
+    },
+    {
+      name: 'News Homepage',
+      description: 'My first level 2 Frontend Mentor challenge. This is a slightly more complex layout made with CSS grid, as well as having a mobile-only hamburger menu.',
+      url: 'news-homepage',
+      imageURL: NewsHomepage
     },
   ]
 
